1fb897e74fd42dfc7d45ae869dc4dfa32bb10697
Luis <B1ON1C@users.noreply.github.com>
2017-07-01 20:58:00 +0200

    Update gonfiguration.go
    New update with new features.

--- gonfiguration.go
+++ gonfiguration.go
@@ -9,46 +9,43 @@ import (
 )
 
 var (
-	errorGonfigurationNil = errors.New("error: gonfiguration is nil")
-	errorCantOpenFile     = errors.New("error: can't open gonfiguration file")
-	errorMapContainsEntry = errors.New("error: map contains that key yet")
-	errorKeyNotFound      = errors.New("error: can't found key-value on gonfiguration map")
+	errorGonfigurationNil = errors.New("gonfiguration object is not initialized")
+	errorCantOpenFile     = errors.New("can't open configuration file")
+	errorDuplicatedEntry  = errors.New("duplicated key")
+	errorKeyNotFound      = errors.New("key doesn't exist")
 )
 
-// Gonfiguration hold info like path, map of values
+// Gonfiguration hold info about the session
 type Gonfiguration struct {
 	GonfigurationValues map[string]string
 	ValidateConfigLine  func(line string) bool
-	FilePath            string
+	Path                string
 }
 
 // isGonfigurationLine internal func for return true if line is correct like key=value
-func (c *Gonfiguration) isGonfigurationLine(line string) bool {
-	if c.ValidateConfigLine != nil {
-		return c.ValidateConfigLine(line)
+func (g *Gonfiguration) isGonfigurationLine(line string) bool {
+	if g.ValidateConfigLine != nil {
+		return g.ValidateConfigLine(line)
 	}
 	return !strings.HasPrefix(line, "#") && line != "" && strings.Contains(line, "=")
 }
 
-// LoadGonfiguration save all key-value into the map
-func (c *Gonfiguration) LoadGonfiguration(path string, validateFunction func(line string) bool) (*Gonfiguration, error) {
+// New return the initialized gonfiguration object
+func New(path string, validateFunction func(line string) bool) (*Gonfiguration, error) {
 	newGonfiguration := new(Gonfiguration)
 	newGonfiguration.GonfigurationValues = make(map[string]string)
 	newGonfiguration.ValidateConfigLine = validateFunction
-	newGonfiguration.FilePath = path
+	newGonfiguration.Path = path
+	errorLoadingGonfiguration := newGonfiguration.LoadFromPath(path)
 
-	reloadError := c.Reload()
-	if reloadError != nil {
-		return nil, reloadError
-	}
-	return newGonfiguration, nil
+	return newGonfiguration, errorLoadingGonfiguration
 }
 
-// Reload clear the map and load all entrys of the config file
-func (c *Gonfiguration) Reload() error {
-	c.Clear()
+// LoadFromPath load the file given without clear the map
+func (g *Gonfiguration) LoadFromPath(path string) error {
+	g.Path = path
 
-	file, err := os.Open(c.FilePath)
+	file, err := os.Open(path)
 	if err != nil {
 		return errorCantOpenFile
 	}
@@ -57,139 +54,181 @@ func (c *Gonfiguration) Reload() error {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		if c.isGonfigurationLine(line) {
-			code := strings.Split(line, "=")[0]
+		if g.isGonfigurationLine(line) {
+			key := strings.Split(line, "=")[0]
 			value := strings.Split(line, "=")[1:]
 
-			if c.ContainsKey(strings.ToLower(code)) {
-				return errorMapContainsEntry
+			contains, _ := g.Contains(key)
+			if contains {
+				return errorDuplicatedEntry
 			}
-			c.GonfigurationValues[strings.ToLower(code)] = strings.Join(value, "=")
+			g.AddNew(strings.ToLower(key), strings.Join(value, "="))
 		}
 	}
 	return nil
 }
 
-// GetParamAsString return the string value of the given key
-func (c *Gonfiguration) GetParamAsString(key string, def string) (string, error) {
-	if c == nil || c.GonfigurationValues == nil {
+// Reload clear the map and call LoadFromPath()
+func (g *Gonfiguration) Reload() error {
+	if g == nil || g.GonfigurationValues == nil {
+		return errorGonfigurationNil
+	}
+
+	g.Clear()
+	errorLoadFromPath := g.LoadFromPath(g.Path)
+	return errorLoadFromPath
+}
+
+// GetParamAsString return string value of the key.
+func (g *Gonfiguration) GetParamAsString(key string, def string) (string, error) {
+	if g == nil || g.GonfigurationValues == nil {
 		return "", errorGonfigurationNil
 	}
 
-	val := c.GonfigurationValues[strings.ToLower(key)]
-	if !c.ContainsKey(strings.ToLower(key)) {
+	val, contains := g.GonfigurationValues[strings.ToLower(key)]
+	if !contains {
 		return def, errorKeyNotFound
 	}
 	return val, nil
 }
 
-// GetParamAsInt return the int value of the given key
-func (c *Gonfiguration) GetParamAsInt(key string, def int) (int, error) {
-	if c == nil || c.GonfigurationValues == nil {
+// GetParamAsStringArray return the value of the key divided by the sep
+func (g *Gonfiguration) GetParamAsStringArray(key, def, sep string) ([]string, error) {
+	if g == nil || g.GonfigurationValues == nil {
+		return nil, errorGonfigurationNil
+	}
+
+	val, contains := g.GonfigurationValues[strings.ToLower(key)]
+	if !contains {
+		return strings.Split(def, sep), errorKeyNotFound
+	}
+	return strings.Split(val, sep), nil
+}
+
+// GetParamAsInt return int value of the key.
+func (g *Gonfiguration) GetParamAsInt(key string, def int) (int, error) {
+	if g == nil || g.GonfigurationValues == nil {
 		return 0, errorGonfigurationNil
 	}
 
-	val := c.GonfigurationValues[strings.ToLower(key)]
-	if !c.ContainsKey(strings.ToLower(key)) {
+	val, contains := g.GonfigurationValues[strings.ToLower(key)]
+	if !contains {
 		return def, errorKeyNotFound
 	}
 	intval, _ := strconv.Atoi(val)
 	return intval, nil
 }
 
-// GetParamAsBool return the bool param of the given key
-func (c *Gonfiguration) GetParamAsBool(key string, def bool) (bool, error) {
-	if c == nil || c.GonfigurationValues == nil {
+// GetParamAsIntArray return the value of the key divided by the sep
+func (g *Gonfiguration) GetParamAsIntArray(key, def, sep string) ([]int, error) {
+	if g == nil || g.GonfigurationValues == nil {
+		return nil, errorGonfigurationNil
+	}
+
+	tmp, errorGetStringArray := g.GetParamAsStringArray(key, def, sep)
+	r := make([]int, len(tmp))
+	for x := 0; x < len(tmp); x++ {
+		num, _ := strconv.Atoi(tmp[x])
+		r[x] = num
+	}
+	return r, errorGetStringArray
+}
+
+// GetParamAsBool return bool value of the key
+func (g *Gonfiguration) GetParamAsBool(key string, def bool) (bool, error) {
+	if g == nil || g.GonfigurationValues == nil {
 		return false, errorGonfigurationNil
 	}
 
-	val := c.GonfigurationValues[strings.ToLower(key)]
-	if !c.ContainsKey(strings.ToLower(key)) {
+	val, contains := g.GonfigurationValues[strings.ToLower(key)]
+	if !contains {
 		return def, errorKeyNotFound
 	}
 	return (val == "true" || val == "1"), nil
 }
 
-// AddNewKeyValueEntry add new entry to the config map, return error if it exists yet
-func (c *Gonfiguration) AddNewKeyValueEntry(key, value string) error {
-	if c == nil || c.GonfigurationValues == nil {
+// AddNew add new entry to the config map, return error if it exists yet
+func (g *Gonfiguration) AddNew(key, value string) error {
+	if g == nil || g.GonfigurationValues == nil {
 		return errorGonfigurationNil
 	}
 
-	if !c.ContainsKey(strings.ToLower(key)) {
-		c.GonfigurationValues[strings.ToLower(key)] = value
-		return nil
+	contains, _ := g.Contains(key)
+	if contains {
+		return errorDuplicatedEntry
 	}
-	return errorMapContainsEntry
-}
-
-// UpdateOrAddEntry add or update the value of the key
-func (c *Gonfiguration) UpdateOrAddEntry(key, value string) error {
-	if c == nil || c.GonfigurationValues == nil {
-		return errorGonfigurationNil
-	}
-	c.GonfigurationValues[strings.ToLower(key)] = value
+	g.GonfigurationValues[strings.ToLower(key)] = value
 	return nil
 }
 
-// DeleteEntry delete a entry from the map
-func (c *Gonfiguration) DeleteEntry(key string) error {
-	if c == nil || c.GonfigurationValues == nil {
+// Update add or update the value of the key
+func (g *Gonfiguration) Update(key, value string) error {
+	if g == nil || g.GonfigurationValues == nil {
 		return errorGonfigurationNil
 	}
-	delete(c.GonfigurationValues, strings.ToLower(key))
+	g.GonfigurationValues[strings.ToLower(key)] = value
 	return nil
 }
 
-// ContainsKey return true if the key exists on the map
-func (c *Gonfiguration) ContainsKey(key string) bool {
-	if c == nil || c.GonfigurationValues == nil {
-		return false
+// Delete remove a entry from the map
+func (g *Gonfiguration) Delete(key string) error {
+	if g == nil || g.GonfigurationValues == nil {
+		return errorGonfigurationNil
 	}
-	_, contains := c.GonfigurationValues[strings.ToLower(key)]
-	return contains
+
+	contains, _ := g.Contains(key)
+	if !contains {
+		return errorKeyNotFound
+	}
+	delete(g.GonfigurationValues, strings.ToLower(key))
+	return nil
+}
+
+// Contains return true if the key exists on the map
+func (g *Gonfiguration) Contains(key string) (bool, error) {
+	if g == nil || g.GonfigurationValues == nil {
+		return false, errorGonfigurationNil
+	}
+
+	_, contains := g.GonfigurationValues[strings.ToLower(key)]
+	return contains, nil
 }
 
 // Clear delete all entrys from the map
-func (c *Gonfiguration) Clear() error {
-	if c == nil || c.GonfigurationValues == nil {
+func (g *Gonfiguration) Clear() error {
+	if g == nil || g.GonfigurationValues == nil {
 		return errorGonfigurationNil
 	}
 
-	for key := range c.GonfigurationValues {
-		delete(c.GonfigurationValues, strings.ToLower(key))
+	for key := range g.GonfigurationValues {
+		delete(g.GonfigurationValues, strings.ToLower(key))
 	}
 	return nil
 }
 
-// ChangePathAndReload change the path of the config file and reload
-func (c *Gonfiguration) ChangePathAndReload(newPath string) error {
-	c.FilePath = newPath
-	reloadError := c.Reload()
-
-	return reloadError
-}
-
-// Dispose delete all entry of the map and set it to nil
-func (c *Gonfiguration) Dispose() {
-	c.GonfigurationValues = nil
-	c.ValidateConfigLine = nil
-	c.FilePath = ""
-}
-
-// GetLen return the len of the config map
-func (c *Gonfiguration) GetLen() (int, error) {
-	if c == nil || c.GonfigurationValues == nil {
+// Len return the len of the config map
+func (g *Gonfiguration) Len() (int, error) {
+	if g == nil || g.GonfigurationValues == nil {
 		return 0, errorGonfigurationNil
 	}
-	return len(c.GonfigurationValues), nil
+
+	return len(g.GonfigurationValues), nil
 }
 
-// GetMap return the map of the Gonfiguration
-func (c *Gonfiguration) GetMap() map[string]string {
-	if c == nil || c.GonfigurationValues == nil {
-		return nil
+// Map return the map of the Gonfiguration
+func (g *Gonfiguration) Map() (*map[string]string, error) {
+	if g == nil || g.GonfigurationValues == nil {
+		return nil, errorGonfigurationNil
 	}
 
-	return c.GonfigurationValues
+	return &g.GonfigurationValues, nil
+}
+
+// Dispose remove all memory used by the object and destroy it
+func (g *Gonfiguration) Dispose() {
+	g.Clear()
+	g.GonfigurationValues = nil
+	g.ValidateConfigLine = nil
+	g.Path = ""
+	g = nil
 }
